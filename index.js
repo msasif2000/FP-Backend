3034e659ecd3b63456b92c48ee26886883b37dc2
msasif2000 <msasifiiuc@gmail.com>
2023-04-26 23:38:55 +0600

    created

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const app = express()
+const port = 3000
+// middle ware
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(`mongodb+srv://booklet:QqcV1NpG1r7dfybE@spartanduo.uuix98d.mongodb.net/?retryWrites=true&w=majority`, {
+
+    useNewUrlParser: true,
+
+    useUnifiedTopology: true,
+
+});
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+
+    console.log(`Connection error: ${err}`);
+
+});
+
+db.once('open', () => {
+
+    console.log('Connected to MongoDB');
+
+});
+
+app.get('/', (req, res) => {
+  res.send('Booklet App is running')
+})
+
+app.listen(port, () => {
+  console.log(`Booklet  on port ${port}`)
+})
